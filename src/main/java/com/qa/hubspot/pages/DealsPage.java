f144edc73125d0415654a372313b71ce6ba7b073
vinodkeshri <vinod.keshri@aeris.net>
2019-08-10 18:41:58 +0530

    created deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,11 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage {
+	
+	public void getDeal(){
+		System.out.println("get deals.");
+	}
+
+}
